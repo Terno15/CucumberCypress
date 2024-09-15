bcf756b6dd6131597393afaa81aabade85c32804
Thierno Diallo <ternodialseny@gmail.com>
2024-09-15 18:36:12 +0200

    Ajout d'un Given

--- cypress/e2e/cucumber-tests/step_definitions/app.js
+++ cypress/e2e/cucumber-tests/step_definitions/app.js
@@ -1,12 +1,38 @@
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
-// Étape pour visiter la page d'accueil
+// Deuxième test réécris d'une autre manière
+// Étape pour ouvrir la page exemple
+Given('I open example page', () => {
+  cy.visit('https://example.com'); // Remplacez par l'URL que vous souhaitez ouvrir
+});
+
+// Vérifier que le titre de la page est visible
+Then('I should see the page title', () => {
+  cy.get('title').should('not.be.empty')// Vérifier que le titre de la page n'est pas vide
+  .contains('Example Domain')
+  cy.get('title').should('contain', 'Example Domain')
+  cy.get('title').should('not.be.visible')
+  cy.get("h1").should("be.visible")
+  cy.get("h1").should("contain", "Example Domain");
+});
+Then('I see a paragraph', () => {
+  cy.get('p').first().should('not.be.empty')// Vérifier que le titre de la page n'est pas vide
+});
+
+//Étape pour visiter la page d'accueil
 Given('je suis sur la page d\'accueil', () => {
   cy.visit('https://www.example.com'); // Changez l'URL si nécessaire
 });
-
 // Étape pour vérifier la présence d'un lien dans un paragraphe
 Then('je devrais voir un lien dans un paragraphe', () => {
-  cy.get('p').find('a').should('be.visible'); // Cherche un lien <a> dans un paragraphe <p>
+  cy.get('p')
+  .first()
+  .find('a')
+  .should('not.exist');  // Vérifier qu'il n'y a pas de lien <a> dans un paragraphe <p>
+  cy.get('p')
+  .last().find('a')
+  .contains('More information...').should('exist')
+  .should('be.visible'); // Cherche un lien <a> dans un paragraphe <p>
 });
+
 
--- cypress/e2e/cucumber-tests/step_definitions/demo.feature
+++ cypress/e2e/cucumber-tests/step_definitions/demo.feature
@@ -3,13 +3,13 @@ Feature: Demo Feature
     Just to create a quick BDD Demo
     this is second line of the description
 
-# Scenario: Should load example website
-#         Given I open example page
-#         Then I see page title
-#         Then I see a paragraph
-#         Then I see another paragraph 'More information...'
+Scenario: Ouvrir la page example.com
+    Given I open example page
+    Then I should see the page title
+    Then I see a paragraph
+    
 
-Scenario: Je vérifie la présence d'un lien dans un paragraphe
+Scenario: Vérifier la présence d'un link dans le paragraphe
     Given je suis sur la page d'accueil
     Then je devrais voir un lien dans un paragraphe
        
