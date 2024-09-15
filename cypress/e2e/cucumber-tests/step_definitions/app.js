f7923b90c6c29fc29cf41f870796f94db3f65e22
Thierno Diallo <ternodialseny@gmail.com>
2024-09-15 22:16:38 +0200

    ajout

--- cypress/e2e/cucumber-tests/step_definitions/app.js
+++ cypress/e2e/cucumber-tests/step_definitions/app.js
@@ -14,6 +14,11 @@ Then('I should see the page title', () => {
   cy.get('title').should('not.be.visible')
   cy.get("h1").should("be.visible")
   cy.get("h1").should("contain", "Example Domain");
+  cy.get('h1').invoke('text').then((text) => {
+    expect(text).to.equal('Example Domain'); // Vérifie que le texte est 'Example Domain'
+  });
+  
+
 });
 Then('I see a paragraph', () => {
   cy.get('p').first().should('not.be.empty')// Vérifier que le titre de la page n'est pas vide
@@ -25,15 +30,8 @@ Given('je suis sur la page d\'accueil', () => {
 });
 // Étape pour vérifier la présence d'un lien dans un paragraphe
 Then('je devrais voir un lien dans un paragraphe', () => {
-  cy.get('p')
-  .first()
-  .find('a')
-  .should('not.exist');  // Vérifier qu'il n'y a pas de lien <a> dans un paragraphe <p>
-  cy.get('p')
-  .last().find('a')
-  .contains('More information...').should('exist')
-  .should('be.visible'); // Cherche un lien <a> dans un paragraphe <p>
-
+  cy.get('p').first().find('a').should('not.exist');  // Vérifier qu'il n'y a pas de lien <a> dans un paragraphe <p>
+  cy.get('p').last().find('a').contains('More information...').should('exist').should('be.visible'); // Cherche un lien <a> dans un paragraphe <p>
   cy.get('a').invoke('attr', 'href').should('eq', 'https://www.iana.org/domains/example')
 });
 
