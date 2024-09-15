d40106d56e1a8e56da33d8a45b0e65754ec5565d
Thierno Diallo <ternodialseny@gmail.com>
2024-09-15 10:11:35 +0200

    first commit

---
+++ cypress/e2e/cucumber-tests/step_definitions/app.js
@@ -0,0 +1,25 @@
+import { defineStep, Given } from "cypress-cucumber-preprocessor/steps";
+
+defineStep("I open example page", () => {
+  cy.visit("https://www.example.com");
+  cy.get('title').contains('Example Domain')
+});
+
+defineStep("I see page title", () => {
+  cy.get('title').should('contain', 'Example Domain')
+  cy.get('title').should('not.be.visible')
+  cy.get("h1").should("be.visible")
+  cy.get("h1").should("contain", "Example Domain");
+});
+defineStep("I see a paragraph", () => {
+  cy.get('p').should('be.visible')
+  cy.get('p').contains('This domain is for use in illustrative examples in documents.')
+});
+
+// defineStep("I see page title {string}", (name) => {
+//   cy.get('title').should('contain', 'Example Domain')
+//   cy.get('title').should('not.be.visible')
+//   cy.get("h1").should("be.visible");
+//   cy.get("h1").should("contain", `${name}`);
+// });
+
---
+++ cypress/e2e/cucumber-tests/step_definitions/demo.feature
@@ -0,0 +1,11 @@
+Feature: Demo Feature
+
+    Just to create a quick BDD Demo
+    this is second line of the description
+
+    Scenario: Should load example website
+        Given I open example page
+        Then I see page title
+        Then I see a paragraph
+       
+        
